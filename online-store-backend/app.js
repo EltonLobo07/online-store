d0e07afb583cf3b16dd01b83a87119234bb0f741
Elton Lobo <eltonsw00@gmail.com>
2022-08-02 22:13:10 +0530

    Add starter code to connect to the DB

--- online-store-backend/app.js
+++ online-store-backend/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const testRouter = require("./controllers/test");
+const logger = require("./utils/logger");
+const { DB_URI } = require("./utils/config");
+const mongoose = require("mongoose");
+
+// Connect to your DB here
+mongoose.connect(DB_URI)
+        .then(() => console.log("Connected to the DB succesfully"))
+        .catch(err => logger.error(err));
 
 const app = express();
 
--- online-store-backend/utils/config.js
+++ online-store-backend/utils/config.js
@@ -3,5 +3,6 @@ require("dotenv").config();
 // Environment variables are now set
 
 const PORT_NUMBER = process.env.PORT_NUMBER;
+const DB_URI = process.env.DB_URI;
 
-module.exports = {PORT_NUMBER};
+module.exports = {PORT_NUMBER, DB_URI};
