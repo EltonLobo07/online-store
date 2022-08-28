7776d4254cc252c556136b086d17c6897238811e
Elton Lobo <eltonsw00@gmail.com>
2022-08-28 13:38:05 +0530

    Add "/anyUnknownPath" styles

--- online-store-frontend/src/components/UnknownPath.js
+++ online-store-frontend/src/components/UnknownPath.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function UnknownPath() {
-    return <div>Nothing here</div>;
+    const navigate = useNavigate();
+
+    return (
+        <div className = "h-full flex flex-col gap-y-2 justify-center items-center border border-gray-100 text-purple-700 text-lg">
+            <div className = "font-medium">
+                Nothing here, head back to the home page using the button below
+            </div>
+
+            <button onClick = {() => navigate("/")} className = "btn">
+                Go to home page
+            </button>
+        </div>
+    );
 };
 
 export default UnknownPath;
